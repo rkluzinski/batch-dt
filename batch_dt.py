4ee2d0b352d80fe5d4a6396b4491e09c6d4a31de
rkluzinski <ryankluzinski@gmail.com>
2018-02-08 11:07:55 -0700

    added script for generating point data.

--- batch_dt.py
+++ batch_dt.py
@@ -1,6 +1,6 @@
 '''
 Written by Ryan Kluzinski
-Last Edited Feb 6, 2018
+Last Edited Feb 7, 2018
 
 A python script that loads xyz point data from a CSV file, computes the
 delaunay triangulation, and then outputs the .obj file of the final mesh.
@@ -8,6 +8,7 @@ delaunay triangulation, and then outputs the .obj file of the final mesh.
 
 import sys
 import numpy as np
+from scipy.spatial import Delaunay
 
 def load_points(infile):
     '''
@@ -19,7 +20,7 @@ def load_points(infile):
                             x2,y2,z2
                             ...
     Returns:
-        points: A n by 3 numpy array containing the points loaded from
+        points: A (n x 3) numpy array containing the points loaded from
             the infile.
     '''
 
@@ -27,8 +28,30 @@ def load_points(infile):
 
     with open(infile, 'r') as f:
         for line in f:
-            point = map(int, line.split(','))
+            point = tuple(map(float, line.split(',')))
             point_list.append(point)
 
-    points = np.asarray(point_list)
+    points = np.array(point_list)
     return points
+
+def write_obj(outfile, points, tri):
+    '''
+    Writes the delaunay triangulation as a .obj file.
+
+    Args:
+        outfile: Filename under which the .obj is written.
+    '''
+
+    with open(outfile, 'w') as f:
+        for x,y,z in points:
+            f.write('v {:.8f} {:.8f} {:.8f}\n'.format(x,y,z))
+        for u,v,w in tri:
+            f.write('f {:d} {:d} {:d}\n'.format(u,v,w))
+
+def main():
+    points = load_points('tests/test100.txt')
+    tri = Delaunay(points[:,[0,1]])
+    write_obj('test.txt', points, tri.simplices)
+
+if __name__ == '__main__':
+    main()
---
+++ test_gen.py
@@ -0,0 +1,21 @@
+'''
+Written by Ryan Kluzinski
+Last Edited Feb 7, 2018
+
+Generates test input files for batch_dt.py.
+'''
+
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Generates a file of 3d points')
+parser.add_argument('outfile', type=argparse.FileType('w'),
+                    help='File for points to be stored in.')
+parser.add_argument('point_num', metavar='N', type=int,
+                    help='The number of points to generate.')
+
+args = parser.parse_args()
+
+points = np.random.rand(args.point_num, 3)
+for x,y,z in points:
+    args.outfile.write("{:.16f}, {:.16f}, {:.16f}\n".format(x,y,z))
